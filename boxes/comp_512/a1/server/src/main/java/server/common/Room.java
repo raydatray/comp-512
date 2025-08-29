1cdbe0168c85bca2644ea56b903c1e91da13ddf1
raydatray <raydatray@gmail.com>
2025-08-29 19:09:34 -0400

    we are so gradled

---
+++ boxes/comp_512/a1/server/src/main/java/server/common/Room.java
@@ -0,0 +1,22 @@
+// -------------------------------
+// adapted from Kevin T. Manley
+// CSE 593
+// -------------------------------
+
+package server.common;
+
+public class Room extends ReservableItem {
+
+    public Room(String location, int count, int price) {
+        super(location, count, price);
+    }
+
+    public String getKey() {
+        return Room.getKey(getLocation());
+    }
+
+    public static String getKey(String location) {
+        String s = "room-" + location;
+        return s.toLowerCase();
+    }
+}
