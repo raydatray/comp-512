b5d0e5a7328c93ea7cafb7440f954f8d24548e6f
aliu07 <>
2025-09-08 16:17:31 -0400

    [a1][shared][3/9] refactor tcp msg formats & refactor client/server layers
    Summary:
    - add payloads for all user interface methods
    - add new tcp response message types
    - mod client with new message types
    - mod server with new message types
    
    Test Plan:

--- boxes/comp_512/a1/shared/lib/tcp/requests/payloads/AddFlight.java
+++ boxes/comp_512/a1/shared/lib/tcp/requests/payloads/AddFlight.java
@@ -1,7 +1,6 @@
-package tcp.payloads;
+package tcp.requests.payloads;
 
 import interfaces.ITCPRequestPayload;
 
 public record AddFlight(int flightNum, int flightSeats, int flightPrice) implements ITCPRequestPayload {
-
 }
---
+++ boxes/comp_512/a1/shared/lib/tcp/requests/payloads/AddRooms.java
@@ -0,0 +1,6 @@
+package tcp.requests.payloads;
+
+import interfaces.ITCPRequestPayload;
+
+public record AddRooms(String location, Integer numRooms, Integer price) implements ITCPRequestPayload {
+}
