5d2cd8bb1dc0c7e31649ab00756b3afedcf0b73f
aliu07 <>
2025-10-25 13:37:59 -0400

    [6/n][a2][writer] add writer component
    Summary:
    
    refer to design hierarchy.
    
    we want Paxos class to be composed of different components. naturally we have:
    - proposer
    - accepter
    
    Each need a reader and a writer component to interface with underlying GCL
    
    this diff provides the primitive implementation for the reader component
    
    Test Plan:
    
    builds and runs at top of stack

---
+++ a2/comp512st/paxos/GCLWriter.java
@@ -0,0 +1,31 @@
+package paxos;
+
+import comp512.gcl.GCL;
+import java.util.List;
+import java.util.logging.Logger;
+
+class GCLWriter {
+
+    private GCL gcl;
+    private Logger logger;
+
+    GCLWriter(GCL gcl, Logger logger) {
+        this.gcl = gcl;
+        this.logger = logger;
+    }
+
+    void send(String destProcess, PaxosMessage msg) {
+        logger.info("Sending message " + msg + " to " + destProcess);
+        gcl.sendMsg(msg, destProcess);
+    }
+
+    void multicast(List<String> processes, PaxosMessage msg) {
+        logger.info("Multicasting message " + msg + " to " + processes);
+        gcl.multicastMsg(msg, processes.toArray(new String[0]));
+    }
+
+    void broadcast(PaxosMessage msg) {
+        logger.info("Broadcasting message " + msg + " to everyone else");
+        gcl.broadcastMsg(msg);
+    }
+}
