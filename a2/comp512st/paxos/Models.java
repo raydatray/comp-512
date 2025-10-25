188b35d31447ff7340433705595eda6d65f395e6
aliu07 <>
2025-10-25 13:37:59 -0400

    [5/n][a2][paxos] add paxos msg and model primitives
    Summary:
    
    in our Paxos protocol, we send different types of message. define a common serializable interface and define records that implement that interface. we can make it sealed too if needed.
    
    app layer sends game moves as an array, encapsulate using model obj. also include models for results of each round since they can return diff types -> e.g. propose can return nothing, or previously committed move, or higher ballot long...
    
    lmk if you have any thoughts on making ts cleaner
    
    Test Plan:
    
    builds and runs at top of stack

---
+++ a2/comp512st/paxos/Models.java
@@ -0,0 +1,24 @@
+package paxos;
+
+import java.io.Serializable;
+
+record GameMove(Integer playerNum, char move) implements Serializable {}
+
+// Phase 1
+
+interface ProposeResult {}
+
+record ProposeSuccess() implements ProposeResult {}
+
+record ProposeSuccessWithMove(GameMove move) implements ProposeResult {}
+
+// someone got more motion than u, use smth greater than their ballot number next time lil bro
+record ProposeFailure(Long greaterBID) implements ProposeResult {}
+
+// Phase 2
+
+interface AcceptResult {}
+
+record AcceptSuccess() implements AcceptResult {}
+
+record AcceptFailure(Long greaterBID) implements AcceptResult {}
