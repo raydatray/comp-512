134b141983a9af2646abcd050b3511e27f6560e0
raydatray <raydatray@gmail.com>
2025-11-02 23:40:08 -0500

    Temporary Commit at 11/2/2025, 11:40:08 PM
    Summary:
    
    Test Plan:

---
+++ a2/comp512st/paxos/Proposer.java
@@ -0,0 +1,66 @@
+package paxos;
+
+import java.util.logging.Logger;
+
+public class Proposer {
+    private GCLReader gclReader;
+    private GCLWriter gclWriter;
+    private Logger logger;
+
+
+    private BallotGenerator ballotGenerator;
+
+    private Integer majority;
+
+    // todo: add a real timeout
+
+    Proposer(
+        GCLReader reader,
+        GCLWriter writer,
+        Logger logger,
+        Integer majority
+    ) {
+        this.gclReader = reader;
+        this.gclWriter = writer;
+        this.logger = logger;
+
+        this.majority = majority;
+    }
+
+    // guarantees i will send this move
+    // run a paxos instance who will return a ProposerState
+    // if its confirmed, and self, we can exit
+    // if not, we need to try again
+    public void sendMessage(Ballot ballotToPropose, GameMove move) {
+        while (true) {
+            ProposerInstance proposerInstance = new ProposerInstance(this.gclReader, this.gclWriter, this.logger, this.majority, ballotToPropose, move);
+
+            Boolean installed = proposerInstance.runInstance(); 
+
+            // if we installed some value, was it what we originally wanted?
+            if (installed) {
+                // we got what we wanted
+                if (proposerInstance.committedOriginalMove()) {
+                    return;
+                } else { 
+                    // we need to try again. 
+                    // update the ballot 
+                    // increment the turn you are fighting for
+                    // increment the ballot
+                    // forward the move
+                    ProposerState state = proposerInstance.getFinalProposerState();
+
+                    // we should tell the proposer state to take the highest ballot + 1
+                    ballotToPropose = ballotGenerator.nextTurn(state.ballotToPropose.turn());
+                    continue;
+                }
+            }
+
+            // if we did not install a value
+            // start the timeout
+            // update our ballot 
+            // do not update the turn yet
+
+        }
+    }
+}
