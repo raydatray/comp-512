1699c396233d5ed094d76260d1d4b5e391891d31
aliu07 <>
2025-11-03 22:23:06 -0500

    [1/n][she][pax] THIS IS THE ONE
    Summary:
    
    I don't remember allat
    
    Test Plan:
    
    PRAY TO GOD

--- a2/comp512st/paxos/Models.java
+++ a2/comp512st/paxos/Models.java
@@ -4,6 +4,26 @@ import java.io.Serializable;
 
 record GameMove(Integer playerNum, Character move) implements Serializable {}
 
+record Ballot(Integer ballotId, Integer playerNum) implements
+    Serializable, Comparable<Ballot> {
+    public int compareTo(Ballot other) {
+        // tie break only by ballotId for now
+        int cmp = this.ballotId.compareTo(other.ballotId);
+        if (cmp != 0) return cmp;
+
+        // then by player number
+        return this.playerNum.compareTo(other.playerNum);
+    }
+
+    Boolean isLessThan(Ballot other) {
+        return this.compareTo(other) < 0;
+    }
+
+    Boolean isGreaterThan(Ballot other) {
+        return this.compareTo(other) > 0;
+    }
+}
+
 // Phase 1
 
 interface ProposeResult {}
@@ -13,7 +33,7 @@ record ProposeSuccess() implements ProposeResult {}
 record ProposeSuccessWithMove(GameMove move) implements ProposeResult {}
 
 // someone got more motion than u, use smth greater than their ballot number next time lil bro
-record ProposeFailure(Long ballotId) implements ProposeResult {}
+record ProposeFailure(Ballot ballot) implements ProposeResult {}
 
 // Phase 2
 
@@ -21,4 +41,4 @@ interface AcceptResult {}
 
 record AcceptSuccess() implements AcceptResult {}
 
-record AcceptFailure(Long ballotId) implements AcceptResult {}
+record AcceptFailure(Ballot ballot) implements AcceptResult {}
