2e4b9f171333cecb7289aa46732ec8d1626ecefb
aliu07 <>
2025-11-19 18:23:22 -0500

    [1/1][a2][misc] I forgot what this diff was for but I think its important
    Summary:
    
    Test Plan:

--- a2/scripts/deploy.sh
+++ a2/scripts/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-source .env
-scp -r ./build/classes/* $(USER)@mimi.cs.mcgill.ca:~/comp-512/a2/build/classes
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+set -a
+source "$SCRIPT_DIR/../.env"
+set +a
+
+scp -r ./build/classes/* "$USER@mimi.cs.mcgill.ca:/home/2023/aliu45/comp-512/a2/build/classes"
